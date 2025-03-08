db183ce22449a8d0a403a8cd081c6223b02d81e4
Ofer K <ofer_k2000@yahoo.com>
2025-03-08 21:28:07 +0200

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShoppingCartComponent } from './views/shopping-cart/shopping-cart.component';
+import { ShoppingCart2Component } from './views/shopping-cart2/shopping-cart2.component';
 
 export const routes: Routes = [
-  { path: '', component:ShoppingCartComponent},
+  { path: '', redirectTo: '/shoppingCart2', pathMatch: 'full' },
   { path: 'shoppingCart', component:  ShoppingCartComponent},
+  { path: 'shoppingCart2', component : ShoppingCart2Component}
 
 ];
