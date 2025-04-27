44448346e650e2a63aa8bb7d72f72650bde122c3
Ofer K <ofer_k2000@yahoo.com>
2025-04-27 21:50:43 +0300

    update the price from parent component

--- src/app/views/price/price.component.ts
+++ src/app/views/price/price.component.ts
@@ -1,13 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, Input, OnInit, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-price',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit {
+  ngOnInit(): void {
+    this.newPrice.set(this.itemPrice);
+
+  }
 
   @Input() itemPrice!: number;
+
+  newPrice = signal<number>(1);
+
+  constructor() {
+    effect(() => {
+      console.log("Price changed to: " + this.newPrice());
+    })
+  }
+
+
+
+
+
+  @Output() priceChanged: (newPrice: number) => void = () => {};
 
 }
